a5d082b02d5c784344e6beae174f591c3b450a55
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2024-03-10 23:24:05 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        DataGridView1 = New DataGridView()
+        ComboBox1 = New ComboBox()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15F, FontStyle.Bold)
+        Label1.Location = New Point(378, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(226, 35)
+        Label1.TabIndex = 0
+        Label1.Text = "CRUD Application"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(104, 94)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(24, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "ID"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(37, 131)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(91, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "FIRST NAME"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(40, 165)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(88, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "LAST NAME"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(62, 205)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(66, 20)
+        Label5.TabIndex = 4
+        Label5.Text = "GENDER"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(155, 93)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(155, 128)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 6
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(155, 165)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 7
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(331, 94)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(125, 27)
+        TextBox4.TabIndex = 8
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(68, 296)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 9
+        Button1.Text = "INSERT"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(186, 296)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 10
+        Button2.Text = "UPDATE"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(483, 94)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 11
+        Button3.Text = "SEARCH"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(599, 94)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 12
+        Button4.Text = "DELETE"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(315, 137)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(378, 188)
+        DataGridView1.TabIndex = 14
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"MALE ", "FEMALE"})
+        ComboBox1.Location = New Point(158, 205)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(122, 28)
+        ComboBox1.TabIndex = 15
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1053, 450)
+        Controls.Add(ComboBox1)
+        Controls.Add(DataGridView1)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ComboBox1 As ComboBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,131 @@
+﻿Imports System.ComponentModel.Design.Serialization
+Imports System.Data.SqlClient
+
+
+
+Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim id As Integer = TextBox1.Text
+        Dim FIRSTNAME As String = TextBox2.Text
+        Dim LASTNAME As String = TextBox3.Text
+        Dim GENDER As String = ComboBox1.SelectedItem
+
+        Dim query As String = "INSERT INTO [Table] VALUES(@ID, @FIRSTNAME, @LASTNAME, @GENDER)"
+        Using con As SqlConnection = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HARSHIL\Documents\crud.mdf;Integrated Security=True;Connect Timeout=30")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                cmd.Parameters.AddWithValue("@ID", id)
+                cmd.Parameters.AddWithValue("@FIRSTNAME", FIRSTNAME)
+                cmd.Parameters.AddWithValue("@LASTNAME", LASTNAME)
+                cmd.Parameters.AddWithValue("@GENDER", GENDER)
+
+                con.Open()
+                cmd.ExecuteNonQuery()
+                con.Close()
+                TextBox1.Text = ""
+                TextBox2.Text = ""
+                TextBox3.Text = ""
+                MessageBox.Show("Successfully Inserted")
+                BindData()
+
+
+            End Using
+        End Using
+    End Sub
+    Public Sub BindData()
+        Dim query As String = "SELECT * FROM [Table]"
+        Using con As SqlConnection = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HARSHIL\Documents\crud.mdf;Integrated Security=True;Connect Timeout=30")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                Using da As New SqlDataAdapter()
+                    da.SelectCommand = cmd
+                    Using dt As New DataTable()
+                        da.Fill(dt)
+                        DataGridView1.DataSource = dt
+
+                    End Using
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim query As String = "SELECT * FROM [Table] WHERE ID = '" & TextBox4.Text & "'"
+        Using con As SqlConnection = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HARSHIL\Documents\crud.mdf;Integrated Security=True;Connect Timeout=30")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                Using da As New SqlDataAdapter()
+                    da.SelectCommand = cmd
+                    Using dt As New DataTable()
+                        da.Fill(dt)
+                        If dt.Rows.Count > 0 Then
+                            TextBox1.Text = dt.Rows(0)(0).ToString()
+                            TextBox2.Text = dt.Rows(0)(1).ToString()
+                            TextBox3.Text = dt.Rows(0)(2).ToString()
+                            ComboBox1.Text = dt.Rows(0)(3).ToString()
+                        Else
+                            TextBox1.Text = ""
+                            TextBox2.Text = ""
+                            TextBox3.Text = ""
+                        End If
+                    End Using
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim id As Integer = TextBox1.Text
+        Dim FIRSTNAME As String = TextBox2.Text
+        Dim LASTNAME As String = TextBox3.Text
+        Dim GENDER As String = ComboBox1.SelectedItem
+
+        Dim query As String = "UPDATE [Table] SET FIRSTNAME = @FIRSTNAME, LASTNAME = @LASTNAME, GENDER = @GENDER WHERE ID = @ID"
+        Using con As SqlConnection = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HARSHIL\Documents\crud.mdf;Integrated Security=True;Connect Timeout=30")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                cmd.Parameters.AddWithValue("@ID", id)
+                cmd.Parameters.AddWithValue("@FIRSTNAME", FIRSTNAME)
+                cmd.Parameters.AddWithValue("@LASTNAME", LASTNAME)
+                cmd.Parameters.AddWithValue("@GENDER", GENDER)
+
+                con.Open()
+                cmd.ExecuteNonQuery()
+                con.Close()
+                TextBox1.Text = ""
+                TextBox2.Text = ""
+                TextBox3.Text = ""
+                MessageBox.Show("Successfully Updated")
+                BindData()
+
+
+            End Using
+        End Using
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Dim id As Integer = TextBox1.Text
+        Dim query As String = "DELETE FROM [Table] WHERE ID = @ID"
+        Using con As SqlConnection = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HARSHIL\Documents\crud.mdf;Integrated Security=True;Connect Timeout=30")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                cmd.Parameters.AddWithValue("@ID", id)
+
+                con.Open()
+                cmd.ExecuteNonQuery()
+                con.Close()
+                TextBox1.Text = ""
+                TextBox2.Text = ""
+                TextBox3.Text = ""
+                MessageBox.Show("Successfully Deleted")
+                BindData()
+
+
+            End Using
+        End Using
+    End Sub
+End Class
